13c3d06bdd0d757a24fb8b5b30044ed83f8aa1c9
Marvin Diaz <marvin.miguel.diaz@gmail.com>
2022-01-06 19:01:21 -0500

    more impport

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,6 +1,13 @@
 from flask import Flask
 from flask_mail import Mail, Message
 import os
+import email
+import emails
+import smtplib
+import email.utils
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
 
 
 
